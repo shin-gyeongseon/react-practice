d4f381800aad966a9c4ebd941811201a5892e867
shinGyeongseon <tlsrid1119@gmail.com>
2024-08-19 14:58:41 +0900

    convert data instanceData to hacknews data

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,5 @@
 import * as React from 'react';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = () =>
-  new Promise((resolve) =>{
-    setTimeout(
-      () => resolve({ data: { stories: initialStories } }),
-      2000
-    );
-  }
-  );
-
 const storiesReducer = (state, action) => {
   switch (action.type) {
     case 'STORIES_FETCH_INIT':
